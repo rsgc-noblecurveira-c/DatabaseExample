fc085c6da7ce277d0ceabc58b4c08b14f12e7dbc
Russell Gordon <rgordon@rsgc.on.ca>
2016-11-08 08:59:01 -0500

    Now initializes database in view controller if it doesn't already exist.

--- DatabaseExample/ViewController.swift
+++ DatabaseExample/ViewController.swift
@@ -15,11 +15,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var phone: UITextField!
     @IBOutlet weak var status: UILabel!
     
-    //
+    // Will save path to database file
+    var databasePath = NSString()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Identify the app's Documents directory and build a path to "contacts.db"
+        let fileManager = FileManager.default
+        let directoryPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = directoryPaths[0]
+        databasePath = documentsDirectory.appending("contacts.db") as NSString
+        
+        // Initialize (create) the database if it doesn't already exist
+        if !fileManager.fileExists(atPath: databasePath as String) {
+            
+            // Create the database
+            let contactDB = FMDatabase(path: databasePath as String)
+            
+            // Check that this worked
+            if contactDB == nil {
+                print("Error: Could not create DB, details: \(contactDB?.lastErrorMessage())")
+            }
+            
+        }
+        
+        
     }
 
     
